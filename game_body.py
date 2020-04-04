976c8b113eec79d3474b70a18f54bf3c92e6e957
Kosia2000 <kosia.babiuch@gmail.com>
2020-04-04 10:22:33 +0200

    def print_underscore

--- game_body.py
+++ game_body.py
@@ -6,9 +6,8 @@ user1 = User()
 game = Game(user1)
 user1.introduce()
 game.random_word()
-game.print_underscore()
 
-for i in range(0,9):
+
+for i in range(0,3):
     user1.choose_letter()
-    user1.different_letters()
     game.check_letters()
--- hangman.py
+++ hangman.py
@@ -12,14 +12,17 @@ class User:
 
     def add_mistake(self):
         self.mistakes += 1
-        
-    def attempt(self):
-        self.used_letters.append(self.letter)
-        
-        
+
     def choose_letter(self):
         print(self.name, "enter letter:")
         self.letter = input()
+
+    def attempt(self):
+        if self.letter in self.used_letters:
+            print("You have already used this letter!")
+        else:
+            self.used_letters.append(self.letter)
+
 
 class Game:
     
@@ -28,18 +31,24 @@ class Game:
     
     def random_word(self):
         #choose random word from list, cut it on letters
+        self.array_with_letters = []
         list_of_words = ["Kosia", "KInka", "KMacieg", "KSiemeg"]
         self.string = random.choice(list_of_words)
-        print("String = ", self.string)
+        #print("String = ", self.string)
+        for word in range(1):
+            self.guess_word = list(self.string)
+            self.array_with_letters.append(self.guess_word)
+        print("Array_with_letters",self.array_with_letters)
+        self.print_underscore()
+
         
     def print_underscore(self):
         for word in range(1):
-            self.guess_word = list(self.string)
             print(len(self.guess_word)* " _")
-
     
     def check_letters(self):            
         print("Guess word:", self.guess_word)
+        self.print_underscore()
         if self.user1.letter in self.guess_word:
             print("YES")
             self.user1.attempt()
@@ -51,15 +60,16 @@ class Game:
             self.user1.attempt()
             print("Used letters:", self.user1.used_letters)
             print("Mistakes:", self.user1.mistakes)
+            
 
         
-user1 = User()
-game = Game(user1)
-user1.introduce()
-game.random_word()
-game.print_underscore()
+#user1 = User()
+#game = Game(user1)
+#user1.introduce()
+#game.random_word()
+#game.print_underscore()
 
-user1.choose_letter()
+#user1.choose_letter()
 
-game.check_letters()
+#game.check_letters()
 
